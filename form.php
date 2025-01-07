e96447eec0bbb1158791d060b5316ae063d4a1a1
Biplab Saha <biplab5523x@gmil.com>
2025-01-07 23:05:40 +0530

    addTheFormCode

--- form.php
+++ form.php
@@ -1,5 +1,26 @@
 <?php
-echo "Hello World!";
+$name=$_POST['n1'];
+$Roll=$_POST['n2'];
+$class=$_POST['n3'];
+$department=$_POST['n4'];
+echo "
+<TABLE border='4'>
+<tr>    
+<td>Name: </td>
+<td>Roll: </td>
+<td>Class: </td>
+<td>Department: </td>
+</tr>
+
+<tr>
+<td>$name</td>
+<td>$Roll</td>
+<td>$class</td>
+<td>$department</td>
+</tr>
+</TABLE>
+";
+
 
 
 ?>
